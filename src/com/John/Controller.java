0a841fd9bd3fe1d9942a7f0f873b7521869fbe72
Johnson Hartanto <54823908+johartanto@users.noreply.github.com>
2019-09-10 16:57:33 +0700

    Tahap 1

---
+++ src/com/John/Controller.java
@@ -0,0 +1,62 @@
+package com.John;
+
+import com.John.Entity.Item;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private TextField txtCatname;
+    @FXML
+    private TableView<Item> tabelList;
+    @FXML
+    private TableColumn col01;
+    @FXML
+    private TableColumn col02;
+    @FXML
+    private TableColumn col03;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtPrice;
+    @FXML
+    private ComboBox cbCat;
+    private ObservableList<Item> items;
+    @FXML
+    private void pressTable(MouseEvent mouseEvent) {
+
+    }
+
+    @FXML
+    private void btnSaveCat(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnUpdate(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnReset(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void btnSave(ActionEvent actionEvent) {
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        items= FXCollections.observableArrayList();
+        tabelList.setItems(items);
+    }
+}
---
+++ src/com/John/Entity/Item.java
@@ -0,0 +1,31 @@
+package com.John.Entity;
+
+public class Item {
+    private String name;
+    private int price;
+    private Category category;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
